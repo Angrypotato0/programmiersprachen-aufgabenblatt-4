dbd7315bae23226cbb24eacb09ebc6e5e96cad82
Elyas Najar <elyas.najar@uni-weimar.de>
2016-06-06 00:30:22 +0200

    :poop:

--- source/List.hpp
+++ source/List.hpp
@@ -1,62 +1,350 @@
-#ifndef BUW_LIST_HPP
+
+
+#ifndef BUW_LIST_HPP 
 #define BUW_LIST_HPP
 
-#include <cstddef>
+#include <cstddef> 
+#include <utility>
 
 template <typename T>
-struct List;
+class List;
 
 template <typename T>
 struct ListNode
-  {
-  	ListNode(): m_value(), m_prev(nullptr), m_next(nullptr){}
-  	ListNode (T const& v, ListNode* prev, ListNode* next): m_value(v), m_prev(prev), m_next(next){}
-	T m_value;
-	ListNode* m_prev ;
-	ListNode* m_next ;
-  };
+ {
+  ListNode() : m_value {}, m_prev {nullptr}, m_next {nullptr} {}
+  ListNode(T const& v, ListNode* prev, ListNode* next) : 
+  m_value{v}, m_prev{prev}, m_next{next} {}
 
-template <typename T>
-struct ListIterator
-  {
-	friend class List <T>;
-	
-
-
-	private:
-	ListNode<T>* m_node=nullptr;
-  }; 
-
-template <typename T>
-struct ListConstIterator
-  {
-	friend class List<T>;
-	public:
-	
-
-
-	private:
-	ListNode<T>* m_node = nullptr ;
-  }; 
-
-template < typename T >
-class List
-  {
-	public:
-	typedef T value_type;
-	typedef T* pointer;
-	typedef const T* const_pointer;
-	typedef T& reference; 
-	typedef const T& const_reference;
-	typedef ListIterator <T> iterator;
-	typedef ListConstIterator <T> const_iterator;
-
-	friend class ListIterator <T>;
-	friend class ListConstIterator <T>;
-
-	private:
-	std::size_t m_size = 0;
-	ListNode <T>* m_first = nullptr;
-	ListNode <T>* m_last = nullptr;
+  T m_value; 
+  ListNode* m_prev; 
+  ListNode* m_next; 
 };
-#endif
+
+template <typename T>
+struct ListIterator 
+{
+  typedef ListIterator<T> Self;
+  typedef T value_type; 
+  typedef T* pointer; 
+  typedef T& reference; 
+  typedef ptrdiff_t difference_type;
+  typedef std::forward_iterator_tag iterator_category;
+
+  friend class List<T>;
+
+  ListIterator() : m_node {nullptr} {}
+  ListIterator(ListNode<T>* n) : m_node {n} {}
+
+  reference operator * () const {
+    return m_node -> m_value; 
+  } 
+
+    
+  pointer operator -> () const { 
+    return &(m_node -> m_value); 
+  } 
+
+    
+  Self& operator ++ () {
+    if (m_node) {
+      m_node = m_node -> m_next; 
+    }
+    return *this; 
+  } 
+
+  Self& operator--() 
+    {
+      m_node = m_node->m_prev;
+      return *this;
+    }
+
+     
+  Self operator ++ (int) {  
+    Self temp = *this; 
+    ++(*this);  
+    return temp;   
+  } 
+
+    
+  bool operator == (const Self& x) const {
+    return m_node == x.m_node;
+  } 
+
+     
+  bool operator != (const Self& x) const {
+    return m_node != x.m_node;
+  } 
+
+  Self next() const {
+    if (m_node)
+      return ListIterator(m_node -> m_next);
+    else
+      return ListIterator(nullptr);
+  }
+
+  Self prev() const {
+    if (m_node)
+      return ListIterator(m_node -> m_prev);
+    else
+      return ListIterator(nullptr);
+  }
+
+private:
+     
+  ListNode<T>* m_node = nullptr;
+};
+
+template <typename T>
+struct ListConstIterator {
+  friend class List<T>;
+
+private:
+  ListNode<T>* m_node = nullptr;
+};
+
+template <typename T>
+class List {
+public: 
+  
+  typedef T value_type;
+  typedef T* pointer;
+  typedef const T* const_pointer;
+  typedef T& reference; 
+  typedef const T& const_reference;
+  typedef ListIterator<T> iterator; 
+  typedef ListConstIterator<T> const_iterator;
+
+  friend class ListIterator<T>;
+  friend class ListConstIterator<T>;
+
+    
+  List(): m_size {0}, m_first {nullptr}, m_last {nullptr} {} 
+
+
+  List(List<T> const& listC): m_size {0}, m_first {nullptr}, m_last {nullptr} {
+    for (iterator i = listC.begin(); i != listC.end(); ++i) {
+      push_back(*i); 
+    }
+  }
+
+    
+  List(List&& listM): m_size {listM.m_size}, m_first {listM.m_first}, m_last {listM.m_last} { // steals list elements from listM
+    listM.m_size = 0;
+    listM.m_first = nullptr;
+    listM.m_last = nullptr; 
+  }
+
+    
+  List<T>& operator = (List<T> list) { 
+    swap(list); 
+    return *this; 
+  }
+
+    
+  void swap(List<T>& list) {
+      std::swap(m_size, list.m_size);
+      std::swap(m_first, list.m_first);
+      std::swap(m_last, list.m_last);
+  }
+
+    
+  friend void swap(List<T>& l1, List<T>& l2) {
+    l1.swap(l2);
+  }
+
+     
+  ~List() {
+    clear(); 
+  }
+
+     
+  bool empty() const {
+    return m_size == 0;
+  }
+
+    
+  std::size_t size() const {
+    return m_size;
+  }
+
+    
+  T const& front() const {
+    return (*m_first).m_value; 
+  }
+
+  T& front() {
+    return (*m_first).m_value; 
+  }
+
+    
+  void push_front(T const& a) {
+    if (m_size == 0) {
+      m_first = new ListNode<T>{a, nullptr, nullptr};
+      m_last = m_first;
+    }
+
+    else if (m_size >= 1) {
+      m_first = new ListNode<T>{a, nullptr, m_first};
+      m_first -> m_next -> m_prev = m_first;
+    }
+
+    ++m_size;
+  }
+
+    
+  void pop_front() {
+    if (m_size == 1) {
+      assert(m_first != nullptr);
+      delete m_first;
+      m_first = nullptr;
+      m_size = 0;
+    }
+
+    else if (m_size > 1) {
+      assert(m_first != nullptr);
+      delete m_first;
+      m_first = m_first -> m_next;
+      --m_size;
+    }
+  }
+
+    
+  T const& last() const {
+    return (*m_last).m_value; 
+  }
+
+  T& last() {
+    return (*m_last).m_value; 
+  }
+
+    
+  void push_back(T const& a) {
+    if (m_size == 0) {
+      m_last = new ListNode<T>{a, nullptr, nullptr};
+      m_first = m_last;
+    }
+
+    else if (m_size >= 1) {
+      m_last = new ListNode<T>{a, m_last, nullptr};
+      m_last -> m_prev -> m_next = m_last;
+    }
+
+    ++m_size;
+  }
+
+    
+  void pop_back() {
+    if (m_size == 1) {
+      assert(m_last != nullptr);
+      delete m_last;
+      m_last = nullptr;
+      m_size = 0;
+    }
+
+    else if (m_size > 1) {
+      assert(m_last != nullptr);
+      delete m_last;
+      m_last = m_last -> m_prev;
+      --m_size;
+    }
+  }
+
+    
+  void clear() {
+    while (m_size > 0) {
+      pop_front();
+    }
+  }
+
+  	
+  iterator begin() const {
+  	return iterator {m_first};
+  }
+
+  	
+  iterator end() const {
+  	return iterator {};
+  }
+
+    
+  void insert(iterator pos, T const& value) {
+
+    if (pos == begin()) { 
+      push_front(value);
+    }
+
+    else if (pos == end()) { 
+      push_back(value);
+    }
+
+    else {
+      ListNode <T>* insertNode = new ListNode<T> {value, pos.prev().m_node, pos.m_node};
+
+      pos.prev().m_node -> m_next = insertNode;
+      pos.m_node -> m_prev = insertNode;
+      ++m_size; 
+    }
+  }
+
+    
+  void reverse() {
+    List<T> tmp{*this}; 
+    clear(); 
+    for (iterator it = tmp.begin(); it != tmp.end(); ++it) {
+      push_front(*it); 
+    }
+  }
+
+
+private:
+  std::size_t m_size = 0;
+  ListNode<T>* m_first = nullptr;
+  ListNode<T>* m_last = nullptr;
+};
+
+
+   
+template<typename T>
+bool operator == (List<T> const& xs, List<T> const& ys) {
+	bool result = true;
+
+	if (xs.size() != ys.size()) { 
+		result = false;
+	}
+
+	else {
+    std::cout << "SIZE CONFIRMED. START SCANNING."<<"\n";
+		ListIterator<T> xs_it = xs.begin(); 
+		ListIterator<T> ys_it = ys.begin();
+
+		while (xs_it != xs.end() && ys_it != ys.end()) { 
+    std::cout << *ys_it <<"   " << *xs_it << "\n"; 
+			if (*xs_it != *ys_it) { 
+				result = false;
+        break;
+			}
+
+			++xs_it; 
+			++ys_it;
+		}
+	}
+
+	return result;
+}
+
+  
+template<typename T>
+bool operator != (List<T> const& xs, List<T> const& ys) {
+	return !(xs == ys);
+}
+  
+  
+template<typename T>
+List<T> reverse (List<T> revList) {
+  revList.reverse();
+  return revList; 
+}
+
+
+#endif // #define BUW_LIST_HPP
---
+++ source/TestDieListMann.cpp
@@ -0,0 +1,440 @@
+
+
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include <cmath>
+#include <algorithm>
+#include "List.hpp"
+
+
+
+TEST_CASE("aufgabe4.1", "[aufgabe4.1]") {
+  List<int> list1 {};
+  REQUIRE(list1.empty() == true);
+}
+
+TEST_CASE("aufgabe4.1", "[aufgabe4.1]") {
+  List<int> list1 {};
+  REQUIRE(list1.size() == 0);
+}
+
+
+TEST_CASE("aufgabe4.2", "[aufgabe4.2]") {
+  List<int> list; 
+  list.push_front(42); 
+  REQUIRE(42 == list.front() );
+
+  List<int> list2;
+  list2.push_front(42);
+  list2.push_front(345);
+  list2.push_front(13);
+  REQUIRE(13 == list2.front());
+
+  List<int> list3;
+  list3.push_front(42);
+  list3.push_front(84);
+  list3.push_front(11);
+  list3.push_front(20);
+  REQUIRE(20 == list3.front());
+} 
+
+TEST_CASE("aufgabe4.2", "[aufgabe4.2]") {
+  List<int> list {}; 
+  list.push_back(42); 
+  REQUIRE(42 == list.last());
+
+  List<int> list2 {};
+  list2.push_back(12);
+  list2.push_back(15);
+  list2.push_back(69);
+  REQUIRE(69 == list2.last());
+
+  List<int> list3 {};
+  list3.push_back(1);
+  list3.push_back(12);
+  list3.push_back(42);
+  list3.push_back(69);
+  REQUIRE(69 == list3.last());
+} 
+
+TEST_CASE("aufgabe4.2", "[aufgabe4.2]") {
+  List<int> list {}; 
+  list.push_back(42); 
+  list.pop_front();
+  REQUIRE(true == list.empty());
+
+  List<int> list1 {};
+  list1.push_back(4);
+  list1.push_back(69);
+  list1.pop_front();
+  REQUIRE(69 == list1.front());
+
+  List<int> list2 {};
+  list2.push_back(42);
+  list2.push_back(98);
+  list2.push_back(8);
+  list2.push_back(65);
+  list2.push_back(4);
+  list2.pop_front();
+  REQUIRE(98 == list2.front());
+} 
+
+TEST_CASE("aufgabe4.2", "[aufgabe4.2]") {
+  List<int> list {}; 
+  list.push_back(42); 
+  list.pop_back();
+  REQUIRE(true == list.empty());
+
+  List<int> list1 {};
+  list1.push_back(42);
+  list1.push_back(69);
+  list1.pop_back();
+  REQUIRE(42 == list1.last());
+
+  List<int> list2 {};
+  list2.push_back(42);
+  list2.push_back(182);
+  list2.push_back(74);
+  list2.push_back(3);
+  list2.pop_back();
+  REQUIRE(74 == list2.last());
+} 
+
+TEST_CASE("aufgabe4.3", "[aufgabe4.3]") {
+  List<int> list; 
+  list.push_front(1); 
+  list.push_front(2); 
+  list.push_front(3); 
+  list.push_front(4); 
+  list.push_front(5);
+  list.push_front(6);
+  list.clear(); 
+  REQUIRE(list.empty());
+
+  List<int> list1; 
+  list1.push_front(30); 
+  list1.push_front(21); 
+  list1.push_front(199); 
+  list1.push_front(136); 
+  list1.push_front(19);
+  list1.push_front(221);
+  list1.clear(); 
+  REQUIRE(list1.empty());
+}
+
+
+TEST_CASE("aufgabe4.5", "[aufgabe4.5]") {
+  List<int> list;
+  auto b = list.begin(); 
+  auto e = list.end(); 
+  REQUIRE(b == e);
+}
+
+TEST_CASE("aufgabe4.5", "[aufgabe4.5]") {
+  List<int> list; 
+  list.push_front(42); 
+  REQUIRE(42 == *list.begin());
+
+  List<int> list1; 
+  list1.push_back(69); 
+  list1.push_back(101);
+  list1.push_back(36);
+  list1.push_back(46);
+  REQUIRE(69 == *list1.begin());
+}
+
+TEST_CASE("aufgabe4.6", "[aufgabe4.6]") {
+  List<int> list1 {};
+  list1.push_back(42); 
+  list1.push_back(69);
+  list1.push_back(101);
+
+  List<int> list2 {};
+  list2.push_back(42);
+  list2.push_back(69);
+  list2.push_back(101);
+  REQUIRE(list1 == list2);
+
+  List<int> list3 {};
+  List<int> list4 {};
+  REQUIRE(list3 == list4);
+
+  List<int> list5 {};
+  list5.push_back(678); 
+  list5.push_back(144);
+  list5.push_back(123);
+  list5.push_back(42);
+  list5.push_back(69);
+  
+  List<int> list6 {};
+  list6.push_back(678); 
+  list6.push_back(144);
+  list6.push_back(123);
+  list6.push_back(42);
+  list6.push_back(69);
+  REQUIRE(list5 == list6);
+}
+
+TEST_CASE("aufgabe4.6", "[aufgabe4.6]") {
+  List<int> list1 {};
+  list1.push_back(42); 
+  list1.push_back(69);
+  list1.push_back(101);
+
+  List<int> list2 {};
+  list2.push_back(42);
+  list2.push_back(4);
+  list2.push_back(101);
+  REQUIRE(list1 != list2);
+
+  List<int> list3 {};
+  list3.push_back(3);
+  List<int> list4 {};
+  REQUIRE(list3 != list4);
+
+  List<int> list5 {};
+  list5.push_back(876); 
+  list5.push_back(441);
+  list5.push_back(234);
+  list5.push_back(24);
+  list5.push_back(96);
+
+
+  List<int> list6 {};
+  list6.push_back(678); 
+  list6.push_back(144);
+  list6.push_back(123);
+  list6.push_back(42);
+  list6.push_back(69);
+  REQUIRE(list5 != list6);
+}
+
+TEST_CASE("aufgabe4.7", "[aufgabe4.7]") {
+  List<int> list; 
+  list.push_front(1); 
+  list.push_front(2); 
+  list.push_front(3); 
+  list.push_front(4); 
+  List<int> l2{list}; 
+  REQUIRE(list == l2);
+
+  List<int> list1; 
+  list1.push_front(69); 
+  list1.push_front(42); 
+  list1.push_front(101); 
+  list1.push_front(264); 
+  list1.push_front(9); 
+  List<int> list2{list1}; 
+  REQUIRE(list1 == list2);
+}
+
+TEST_CASE("aufgabe4.8", "[aufgabe4.8]") {
+  List<int> list; 
+  list.push_front(69); 
+  list.push_front(42); 
+  list.push_front(101); 
+  list.push_front(264); 
+  list.push_front(9); 
+  list.insert(list.begin(), 13);
+  REQUIRE(6 == list.size());
+  REQUIRE(13 == list.front());
+  REQUIRE(69 == list.last());
+
+  List<int> list2; 
+  list2.push_front(69); 
+  list2.push_front(42); 
+  list2.push_front(101); 
+  list2.push_front(264); 
+  list2.push_front(9); 
+  list2.insert(list2.end(), 13);
+  REQUIRE(6 == list2.size());
+  REQUIRE(9 == list2.front());
+  REQUIRE(13 == list2.last());
+
+  List<int> list3; 
+  list3.push_front(69); 
+  list3.push_front(42); 
+  list3.push_front(101); 
+  list3.push_front(264); 
+  list3.push_front(9); 
+  ListIterator<int> pos = list3.begin();
+  pos++; 
+  ++pos;
+  ++pos; 
+  list3.insert(pos, 13);
+  REQUIRE(6 == list3.size());
+  REQUIRE(9 == list3.front());
+  REQUIRE(69 == list3.last());
+  ListIterator<int> it = list3.begin();
+  REQUIRE(9 == *it); 
+  ++it; 
+  REQUIRE(264 == *it); 
+  ++it;
+  REQUIRE(101 == *it); 
+  ++it;
+  REQUIRE(13 == *it); 
+}
+
+TEST_CASE("aufgabe4.9", "[aufgabe4.9]") {
+    
+  List<int> list; 
+  list.push_back(1); 
+  list.push_back(2); 
+  list.push_back(3); 
+  list.push_back(4); 
+  list.push_back(5);
+  list.reverse();
+  ListIterator<int> it = list.begin();
+  REQUIRE(5 == *it);
+  ++it;
+  REQUIRE(4 == *it);
+  ++it; 
+  REQUIRE(3 == *it);
+  ++it;
+  REQUIRE(2 == *it);
+  ++it; 
+  REQUIRE(1 == *it);
+
+    
+  reverse(list);
+  ListIterator<int> i = list.begin();
+  REQUIRE(5 == *i);
+  ++i;
+  REQUIRE(4 == *i);
+  ++i; 
+  REQUIRE(3 == *i);
+  ++i;
+  REQUIRE(2 == *i);
+  ++i; 
+  REQUIRE(1 == *i);
+
+}
+
+TEST_CASE("aufgabe4.10", "[aufgabe4.10]") {
+  List<int> list; 
+  list.push_back(1); 
+  list.push_back(2); 
+  list.push_back(3); 
+  list.push_back(4); 
+  list.push_back(5);
+  std::vector<unsigned int> viktor(list.size());
+  std::copy(list.begin(), list.end(), std::begin(viktor));
+  
+  REQUIRE(1 == viktor[0]);
+  REQUIRE(2 == viktor[1]);
+  REQUIRE(3 == viktor[2]);
+  REQUIRE(4 == viktor[3]);
+  REQUIRE(5 == viktor[4]);
+}
+
+TEST_CASE("aufgabe4.11", "[aufgabe4.11]") {
+    
+  List<int> list1 {}; 
+  list1.push_back(1); 
+  list1.push_back(2); 
+  list1.push_back(3); 
+  list1.push_back(4); 
+  list1.push_back(5);
+
+  List<int> list2 {}; 
+  list2.push_back(10); 
+  list2.push_back(20); 
+  list2.push_back(30); 
+  list2.push_back(40); 
+  list2.push_back(50);
+
+  list1.swap(list2);
+  ListIterator<int> i = list1.begin();
+  REQUIRE(10 == *i);
+  ++i;
+  REQUIRE(20 == *i);
+  ++i; 
+  REQUIRE(30 == *i);
+  ++i;
+  REQUIRE(40 == *i);
+  ++i; 
+  REQUIRE(50 == *i);
+
+    
+  List<int> list5 {}; 
+  list5.push_back(1); 
+  list5.push_back(2); 
+  list5.push_back(3); 
+  list5.push_back(4); 
+  list5.push_back(5);
+
+  List<int> list6 {}; 
+  list6.push_back(10); 
+  list6.push_back(20); 
+  list6.push_back(30); 
+  list6.push_back(40); 
+  list6.push_back(50);
+
+  swap(list5, list6);
+  ListIterator<int> iter = list5.begin();
+  REQUIRE(10 == *iter);
+  ++iter;
+  REQUIRE(20 == *iter);
+  ++iter; 
+  REQUIRE(30 == *iter);
+  ++iter;
+  REQUIRE(40 == *iter);
+  ++iter; 
+  REQUIRE(50 == *iter);
+
+  ListIterator<int> itera = list6.begin();
+  REQUIRE(1 == *itera);
+  ++itera;
+  REQUIRE(2 == *itera);
+  ++itera; 
+  REQUIRE(3 == *itera);
+  ++itera;
+  REQUIRE(4 == *itera);
+  ++itera; 
+  REQUIRE(5 == *itera);
+
+    
+  List<int> list3 {};
+  list3.push_back(1); 
+  list3.push_back(2); 
+  list3.push_back(3); 
+  list3.push_back(4); 
+  list3.push_back(5);
+
+  List<int> list4 {}; 
+
+  list4 = list3;
+  ListIterator<int> ite = list4.begin();
+  REQUIRE(1 == *ite);
+  ++ite;
+  REQUIRE(2 == *ite);
+  ++ite; 
+  REQUIRE(3 == *ite);
+  ++ite;
+  REQUIRE(4 == *ite);
+  ++ite; 
+  REQUIRE(5 == *ite);
+
+}
+
+TEST_CASE("aufgabe4.12", "[aufgabe4.12]") {
+  List<int> list;
+  list.push_front(1);
+  list.push_front(2);
+  list.push_front(3);
+  list.push_front(4);
+
+  List<int> list2(std::move(list));
+  REQUIRE(0 == list.size());
+  REQUIRE(list.empty());
+  REQUIRE(4 == list2.size());
+}
+
+
+
+int main(int argc, char * argv[]) {
+
+  return Catch::Session().run(argc, argv);
+
+}
+
